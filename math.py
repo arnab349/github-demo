cfc654e97bd9c53a203566cf25f9998de586327a
arnab349 <68004410+arnab349@users.noreply.github.com>
2022-01-30 20:39:25 +0530

    Initial Commit of math.py

---
+++ math.py
@@ -0,0 +1,15 @@
+# Add implementation
+def add(x,y):
+	return x+y
+
+# Substract implementation
+def subtract(x,y):
+	return x-y          #onmaster branch
+
+# Multiply implementation
+def multiply(x,y):
+	return x*y          #on bug456 branch
+
+# Divide implementation
+def divide(x,y):
+	pass
